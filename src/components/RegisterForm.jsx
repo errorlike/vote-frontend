9b06882c90ffb218f12548b2984eb32c4d5a4ff3
errorlike <errorlikeman@gmail.com>
2023-02-22 01:41:16 +0800

    change icon color change image

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import authService from '../services/auth';
+import cover from '../assets/cover.jpg';
 const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [matchingPassword, setMatchingPassword] = useState('');
-  const [age, setAge] = useState('');
+  const [age, setAge] = useState(0);
   const [email, setEmail] = useState('');
 
   const onSubmit = async (event) => {
@@ -13,7 +15,7 @@ const RegisterForm = () => {
     setUsername('');
     setPassword('');
     setMatchingPassword('');
-    setAge('');
+    setAge(0);
     setEmail('');
   };
   return (
@@ -21,8 +23,8 @@ const RegisterForm = () => {
       <div className='lg:grid lg:min-h-screen lg:grid-cols-12'>
         <aside className='relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6'>
           <img
-            alt='Pattern'
-            src='https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+            alt='vote'
+            src={cover}
             className='absolute inset-0 h-full w-full object-cover'
           />
         </aside>
@@ -32,7 +34,7 @@ const RegisterForm = () => {
           className='flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6'
         >
           <div className='max-w-xl lg:max-w-3xl'>
-            <a className='block text-blue-600' href='/'>
+            <Link className='block text-indigo-600' href='/'>
               <span className='sr-only'>Home</span>
               <svg
                 className='h-8 sm:h-10'
@@ -45,7 +47,7 @@ const RegisterForm = () => {
                   fill='currentColor'
                 />
               </svg>
-            </a>
+            </Link>
 
             <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl'>
               Welcome to Vote
@@ -132,24 +134,26 @@ const RegisterForm = () => {
                 </label>
 
                 <input
-                  type='text'
+                  type='number'
                   id='age'
                   name='age'
+                  min={0}
+                  max={120}
                   value={age}
                   onChange={event => setAge(event.target.value)}
                   className='mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm'
                 />
               </div>
               <div className='col-span-6 sm:flex sm:items-center sm:gap-4'>
-                <button className='inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500'>
+                <button className='inline-block shrink-0 rounded-md border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500'>
                   Create an account
                 </button>
 
                 <p className='mt-4 text-sm text-gray-500 sm:mt-0'>
                   Already have an account?
-                  <a href='#' className='text-gray-700 underline'>
+                  <Link to='/login' className='text-gray-700 underline'>
                     Log in
-                  </a>
+                  </Link>
                   .
                 </p>
               </div>
