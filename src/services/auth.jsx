b4a8e61406243757da02c22620b428c3b0739c2a
errorlike <errorlikeman@gmail.com>
2023-03-04 21:17:20 +0800

    Modify the file suffix to avoid  VSCODE from being debugged

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,11 +1,11 @@
 import client from '../config/client';
 
 const login = async (loginObject) => {
-  const response = await client.post('/auth/login', loginObject);
+  const response = await client.post('/auth/login', loginObject, { headers: {} });
   return response.data;
 };
 const register = async (registerObject) => {
-  const response = await client.post('/auth/register', registerObject);
+  const response = await client.post('/auth/register', registerObject, { headers: {} });
   return response;
 };
 const authService = {
